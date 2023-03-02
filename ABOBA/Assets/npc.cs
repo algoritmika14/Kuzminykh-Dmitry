79e37d6667db10fb9a6e492ba60eb4368689f797
algoritmika14 <algomklass@gmail.com>
2023-03-02 11:17:44 +0300

    sum1

---
+++ ABOBA/Assets/npc.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Здоровье NPS
+    public int health = 5
+
+    //Уровень NPS
+    public int level = 1
+
+    //Скорость NPS
+    public float speed = 1.2f\
+
+        newPosition.z += speed* Time.deltaTime;
+
+    transform.position
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
